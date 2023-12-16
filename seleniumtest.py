43315815e17e667dab934939e74baa7e6e5691b5
milas-melt <tlemcani.salim00@gmail.com>
2023-12-16 20:36:38 +0000

    selenium setup

--- seleniumtest.py
+++ seleniumtest.py
@@ -1,13 +1,30 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 
-options = Options()
-options.add_experimental_option("detach", True)
+# Path to the Chromedriver
+chromedriver_path = "./chromedriver-mac-x64/chromedriver"
 
-driver = webdriver.Chrome(
-    service=Service(ChromeDriverManager().install()), options=options
-)
+# Create a Service object passing the path of the Chromedriver
+service = Service(chromedriver_path)
 
-driver.get("https://www.neuraline.com/")
+# Initialize the Chrome Driver with the Service object
+driver = webdriver.Chrome(service=service)
+
+# Open Google
+driver.get("http://www.google.com")
+
+# Find the search box
+search_box = driver.find_element_by_name("q")
+
+# Type in a search query
+search_box.send_keys("Hello World")
+
+# Wait for the results to load
+driver.implicitly_wait(5)
+
+# Print the titles of the search results
+for title in driver.find_elements_by_xpath("//h3"):
+    print(title.text)
+
+# Close the browser
+driver.quit()
