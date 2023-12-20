2838bc0ccca9bafbfefcff20776f90e11decf8e6
milas-melt <tlemcani.salim00@gmail.com>
2023-12-20 18:57:32 +0100

    subfolder added to data processor + edits in plan and readme

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ API starter: `uvicorn api_creation_test:app --reload`
 [Click here](https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/120.0.6099.109/mac-x64/chromedriver-mac-x64.zip) to download chrome webdriver for selenium (assuming user is user chrome latest version @wed 20 dec 2023)
 
 for data_processor: make sure `xlrd` module is installed
+
+workflow: install packages & setup => `data_get_request.py` => `data_processor.py`
--- data_processor.py
+++ data_processor.py
@@ -7,12 +7,13 @@ Data Processor Script for Excel Files
 
 This script processes Excel files stored in various thematic folders within a 'data' directory.
 It converts each Excel file (with '.xls' or '.xlsx' extension) into a CSV format and saves
-the output in a 'csv_data' directory. If the 'csv_data' directory does not exist, it is created,
-and if it does exist, its contents are cleared before new files are added.
+the output in corresponding thematic subfolders within a 'csv_data' directory. If the 'csv_data'
+directory or its subdirectories do not exist, they are created.
 
 The script processes each thematic folder within the 'data' directory. Within each folder,
 it identifies Excel files, reads them into a pandas DataFrame, and then saves them as CSV files.
-Each CSV file is named after the original Excel file. 
+Each CSV file is named after the original Excel file and is stored in the corresponding thematic
+subfolder in 'csv_data'.
 
 The script is structured to handle and skip non-directory files encountered in the 'data' directory 
 and also to skip files within each thematic folder that do not have an Excel file extension.
@@ -31,28 +32,22 @@ Note:
     - This script is compatible with Python 3.8 or later.
 """
 
-# Create csv_data directory if it doesn't exist, or clear it if it does
+# Create csv_data directory if it doesn't exist
 csv_data_path = "csv_data"
 if not os.path.exists(csv_data_path):
     os.makedirs(csv_data_path)
-else:
-    # Clear existing files in the directory
-    for filename in os.listdir(csv_data_path):
-        file_path = os.path.join(csv_data_path, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print(f"Failed to delete {file_path}. Reason: {e}")
 
-# Rest of your code
 for theme in os.listdir("data"):
     theme_path = os.path.join("data", theme)
 
     if os.path.isdir(theme_path):
-        print(f"Processing {theme}")
+        print(f"Processing theme: {theme}")
+
+        # Create subdirectory for theme in csv_data if it doesn't exist
+        theme_csv_dir = os.path.join(csv_data_path, theme)
+        if not os.path.exists(theme_csv_dir):
+            os.makedirs(theme_csv_dir)
+
         for filename in os.listdir(theme_path):
             print(f"Processing {filename}")
             file_path = os.path.join(theme_path, filename)
@@ -61,10 +56,10 @@ for theme in os.listdir("data"):
             if file_extension in ["xls", "xlsx"]:
                 df = pd.read_excel(file_path)
                 csv_filename = os.path.splitext(filename)[0] + ".csv"
-                csv_path = os.path.join(csv_data_path, csv_filename)
+                csv_path = os.path.join(theme_csv_dir, csv_filename)
                 df.to_csv(csv_path, index=False)
                 print(f"Done with {filename}")
             else:
-                continue
+                print(f"Skipping non-Excel file: {filename}")
     else:
-        print(f"Skipping non-directory {theme}")
+        print(f"Skipping non-directory: {theme}")
